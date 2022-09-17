01241d5c05083d655bff552a7cfeb46f675daa8f
Calle Englund <git@notcalle.xyz>
2022-09-17 16:04:24 +0200

    Upgrade gyro to 0.7.0

--- Formula/gyro.rb
+++ Formula/gyro.rb
@@ -3,17 +3,22 @@ class Gyro < Formula
   homepage "https://astrolabe.pm"
   license "MIT"
   depends_on "zig"
-  version "0.5.0"
+  version "0.7.0"
 
   stable do
     case Hardware::CPU.arch
       when :arm64
         url "https://github.com/mattnite/gyro/releases/download/#{version}/gyro-#{version}-macos-aarch64.tar.gz"
-        sha256 "878a60a9385afb6c9f24d4048007acb382f09d69c375d39bc1cb43d75f1c1797"
+        sha256 "c8fbfb83fdac6df6169f805563daeafb9abaa8446b30c7c6a7bccf15fd1da449"
       when :x86_64
         url "https://github.com/mattnite/gyro/releases/download/#{version}/gyro-#{version}-macos-x86_64.tar.gz"
-        sha256 "d2604fdb5ad810917532d58a563e39eef1adba5fcd996268e0f90404dab62849"
+        sha256 "b50e7c936718eef6aee0830371f368fe20008d0225eb34ad7ad4fb42dbf9e26c"
     end
+  end
+
+  livecheck do
+    url "https://github.com/mattnite/gyro.git"
+    regex /^(\d+\.\d+\.\d+)$/
   end
 
   head do
@@ -22,7 +27,7 @@ class Gyro < Formula
 
   def install
     if head?
-      system "zig build install --prefix #{prefix}"
+      system "zig", "build", "install", "--prefix", prefix
     else
       bin.install "bin/gyro"
     end
